54d729b79fb34ccbd1865ba75e192bcd6695d67c
Petingoso <petingavasco@protonmail.com>
2022-02-04 21:50:13 +0000

    Ask efi stuff in script

--- archscript.sh
+++ archscript.sh
@@ -1,9 +1,25 @@
 #!/bin/sh -e
-echo "This is my shitty arch install script"
+echo "This is my shitty arch install script, caps sensisitive so pls follow the stuff"
 
-timedatectl set-ntp true && echo "time set with sucess"#sets time
+timedatectl set-ntp true && echo "time set with sucess" #Sets time on ISO
 
-echo "If on GPT, say 1; if MBR say 0"
+echo "If on GPT, say 1; if MBR say 0" #Ask for GPT and store
 read GPT
 echo "$GPT"
+
+echo "What disk do you want to use?"
+read disk
+
+
+if $GPT==0
+	echo"Skipping efi"
+else
+	echo"Make new efi partition? [y/n]"
+	read efi_new
+
+	if efi_new == 'n'
+		echo "What's your efi partition?[/dev/disk1]"
+		read efi_path
+	fi
+fi
 
