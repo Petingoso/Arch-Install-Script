7066ad5308d311c283e2d1ff9b0ccac2a8da8641
Petingoso <petingavasco@protonmail.com>
2022-02-04 12:35:03 +0000

    fuck partitoning

--- archscript.sh
+++ archscript.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -e
 echo "This is my shitty arch install script"
-timedatectl set-ntp true && echo "time set with sucess"
+
+timedatectl set-ntp true && echo "time set with sucess"#sets time
+
+echo "If on GPT, say 1; if MBR say 0"
+read GPT
+echo "$GPT"
+
