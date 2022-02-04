e9531ad5c35f863e1f0ad872054c14b2e2dcb1fe
Petingoso <petingavasco@protonmail.com>
2022-02-04 12:27:51 +0000

    fuck

--- archscript.sh
+++ archscript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 echo "This is my shitty arch install script"
-exec timedatectl set-ntp true && echo "time set with sucess"
+exec "timedatectl set-ntp true && echo "time set with sucess""
